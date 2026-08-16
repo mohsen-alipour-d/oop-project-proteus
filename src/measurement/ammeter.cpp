911a65e03aabc3ea10d94152836e16766de38d06
Mohsen Alipour <mohsenalipoor.d@gmail.com>
2026-08-16 22:13:36 +0330

    make ammeter an ideal conductor so it reads true series current

--- src/measurement/ammeter.cpp
+++ src/measurement/ammeter.cpp
@@ -6,14 +6,21 @@
 Ammeter::Ammeter(string name, double x, double y) : Component(name, x, y) {
     addPin(-10, 0);
     addPin(10, 0);
+    pins[0].setDirection(PinDirection::Input);
+    pins[1].setDirection(PinDirection::Bidirectional);
 }
 
 void Ammeter::step(double dt, double simTime) {
     reading = 0;
-    if (!pins[0].connected || !pins[1].connected)
+    if (!pins[0].connected || !pins[1].connected) {
+        pins[1].setDriving(false);
         return;
+    }
     if (!host)
         return;
+
+    pins[1].setDriving(true);
+    pins[1].voltage = pins[0].voltage;
 
     for (Net* n : host->nets) {
         if (!n->hasPin(&pins[0]) && !n->hasPin(&pins[1]))
